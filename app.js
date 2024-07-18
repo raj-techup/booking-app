51f8bd5f8c6bf313f93db5fb64a3da41c7943292
raj-techup <raj.techup@gmail.com>
2024-07-18 19:27:04 +0530

    commit

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
-/*
-Copyright 2021 Square Inc.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 
 const express = require("express");
 const logger = require("morgan");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
+const serverless = require('serverless-http');
 
 const app = express();
 
@@ -131,5 +118,13 @@ app.use(function (err, req, res, next) {
   });
 });
 
+
+const port = 8080;
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// module.exports.handler = serverless(app);
 
 module.exports = app;
